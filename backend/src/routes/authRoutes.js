64e2be344413318209b9de3ffa7362ef8ba904c4
Rei Mici <rei.mici2@studio.unibo.it>
2025-12-02 09:27:07 +0100

    Removed Backend Folder

--- backend/src/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-// Definizione della rotta POST /api/auth/register
-router.post('/register', authController.register);
-
-module.exports = router;
